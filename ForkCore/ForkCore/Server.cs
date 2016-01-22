1a492d9aff05535ac2e1b4901ff8d625903b4a9d
cmcginn <Urbana@000!0>
2016-01-22 16:04:44 -0500

    Added Fork Core

---
+++ ForkCore/ForkCore/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkCore
+{
+    public class Server:IServer
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
